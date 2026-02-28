04dfb4061a074d24de70ff433871eff0ea155b28
Maria Antipova <experiment0@yandex.ru>
2026-02-28 20:57:21 +0300

    Изменен вывод в select при создании задачи

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -1,10 +1,18 @@
+from django.contrib.auth.models import User
 from django.forms import ModelForm
 
 from task_manager.tasks.models import Task
 
 
 class TaskForm(ModelForm):
-
+    def __init__(self, *args, **kwargs):
+        super(ModelForm, self).__init__(*args, **kwargs)
+        
+        self.fields["executor"].queryset = User.objects.all()
+        self.fields["executor"].label_from_instance = \
+            lambda obj: obj.get_full_name()
+        
     class Meta:
         model = Task
-        fields = ["status", "name", "description", "executor", "labels"]
+        fields = ["name", "description", "status", "executor", "labels"]
+
