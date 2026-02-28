8d23a6ef28c8ff901c7f8f16be3f34fde521af29
Maria Antipova <experiment0@yandex.ru>
2026-02-28 08:29:15 +0300

    Проверяем гипотезу, что playwright не докручивает страницу до select из за большого textarea

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, Textarea
 
 from task_manager.tasks.models import Task
 
@@ -8,3 +8,6 @@ class TaskForm(ModelForm):
     class Meta:
         model = Task
         fields = ["name", "description", "status", "executor", "labels"]
+        widgets = {
+          'description': Textarea(attrs={'rows': 3, 'cols': 40}),
+        }
